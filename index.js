758792e7c0e872114a9cfb481f6fed222243f012
pir4cy <kt145@hotmail.com>
2020-10-03 02:45:06 +0530

    Fixed system

--- index.js
+++ index.js
@@ -3,39 +3,44 @@ const config = require("./config.json");
 
 const client = new Discord.Client();
 const prefix = ";";
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
+
+let admin = '';
 
 client.on("message", message => {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
    
     if (message.content === `${prefix}server`) {
-        message.channel.send(`${message.guild.name} is gey`)
-    } 
-    else if (message.content === `${prefix}mute`){ 
-        if (message.member.voice.channel) {
-            let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-            for (const [memberID, member] of channel.members){
-                if (member != message.member)
-                    member.voice.setMute(true);
-            }
-            console.log("Muted")
+        console.log(`${message.guild.name}`)
+    } else if (message.content === `${prefix}play`){ 
+        if(message.member.hasPermission(`MUTE_MEMBERS`)){
+            message.react('🤫')
+            message.send(``)
+            admin = message.member.id;
+            console.log(admin);    
         }
-    }  
-    else if (message.content === `${prefix}unmute`){
-        if (message.member.voice.channel) {
-            let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-            for (const [memberID, member] of channel.members){
-                if (member != message.member)
-                    member.voice.setMute(false);
-            }
-            console.log("Unmuted")
+    } else if (message.content === `${prefix}over`){
+        if(message.member.hasPermission(`MUTE_MEMBERS`)){
+            message.react('🤡')
+            admin = '';
         }
     }
-    else {
-        message.channel.send(`You need to join a voice channel first`)
+})
+
+client.on('voiceStateUpdate', async(oldState, newState) => {
+    let userID = oldState.member.id;
+    if (userID === admin){
+        console.log('Voice State Changed')
+        let channel = oldState.guild.channels.cache.get(oldState.member.voice.channel.id);
+        for (const [memberID, member] of channel.members) {
+            if (member != oldState.member){
+                member.voice.setMute(oldState.member.voice.mute);
+            }
+        }
     }
 })
 
